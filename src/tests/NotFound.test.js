251a2644b9c78e821df9149355352fbd8ad4cd16
Thaysa <thay.fernnanda@gmail.com>
2022-07-20 17:02:18 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import NotFound from '../pages/NotFound';
+
+describe('Testando NotFound', () => {
+  test('Teste se a página contém um heading', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+    const headingNotFound = screen.getByRole('heading', {
+      name: /Page requested not found/i, level: 2 });
+    expect(headingNotFound).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+    const imgNotFound = screen.getByAltText(/pikachu crying/i);
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
