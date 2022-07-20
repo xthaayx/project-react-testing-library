10e671863988f337335a820548d69edb863d7852
Thaysa <thay.fernnanda@gmail.com>
2022-07-20 13:27:30 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Testando App', () => {
+  test('Teste se o topo da aplicação contém links de navegação', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavorites = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(linkFavorites).toBeInTheDocument();
+  });
+});
